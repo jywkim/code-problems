7163ec9b82ce32098e1ae98ada18c20ae436ca30
jywkim <jkim589@gmail.com>
2018-02-28 13:44:27 -0500

    fizz buzz solution

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -34,3 +34,17 @@
 // var fizzBuzz = function(n) {
 //
 // };
+var fizzBuzz = function(n) {
+  for (var i = 1; i <= n; i++) {
+    if (i % 15 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+console.log(fizzBuzz(15));
