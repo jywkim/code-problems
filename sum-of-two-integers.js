c50a72332acc97290a6120b69d760da599ac5d77
jywkim <jkim589@gmail.com>
2018-03-02 15:41:20 -0500

    sum of two integers solution

--- sum-of-two-integers.js
+++ sum-of-two-integers.js
@@ -14,3 +14,18 @@
 // var getSum = function(a, b) {
 //
 // };
+var getSum = function(a, b) {
+  if (b == 0) {
+    return a
+  };
+  if (a == 0) {
+    return b
+  };
+  while (b != 0) {
+    var carry = a & b;
+    a = a ^ b;
+    b = carry << 1;
+  }
+  return a;
+};
+console.log(getSum(1, 2));
