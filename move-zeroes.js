c9f8f4e14b0380818f7d3f9d89de2cac8db7e5da
jywkim <jkim589@gmail.com>
2018-03-02 13:23:08 -0500

    move zeroes solution

--- move-zeroes.js
+++ move-zeroes.js
@@ -15,3 +15,17 @@
 // var moveZeroes = function(nums) {
 //
 // };
+var moveZeroes = function(nums) {
+  var index = 0;
+  for (var i = 0; i < nums.length; i++) {
+    var n = nums[i];
+    if (n !== 0) {
+      nums[index++] = n;
+    }
+  }
+  for (index; index < nums.length; index++) {
+    nums[index] = 0;
+  }
+  return nums;
+};
+console.log(moveZeroes([0, 1, 0, 3, 12]));
