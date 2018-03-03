b3a688c2880051aea30eb3f2445e398b3b532e5c
jywkim <jkim589@gmail.com>
2018-03-02 23:57:19 -0500

    majority element solution

--- majority-element.js
+++ majority-element.js
@@ -11,3 +11,20 @@
 // var majorityElement = function(nums) {
 //
 // };
+var majorityElement = function(nums) {
+  var map = {};
+  if (nums.length === 1) {
+    return nums[0]
+  }
+  for (var i = 0 ; i < nums.length ; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else {
+      map[nums[i]]++;
+      if (map[nums[i]] >= nums.length/2) {
+        return nums[i];
+      }
+    }
+  }
+};
+console.log(majorityElement([1, 4, 4, 4, 3]));
