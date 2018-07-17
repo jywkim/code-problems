71e46ee51dfcde5fbf16c95595e4ce0416747931
jywkim <jkim589@gmail.com>
2018-07-16 21:50:22 -0400

    merge two sorted lists solution

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -21,3 +21,44 @@
 // var mergeTwoLists = function(l1, l2) {
 //
 // };
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var mergeTwoLists = function(l1, l2) {
+  var result = new ListNode(null, null);
+  var c = result;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val > l2.val) {
+      c.next = l2;
+      l2 = l2.next;
+    } else {
+      c.next = l1;
+      l1 = l1.next
+    }
+  c = c.next;
+  }
+
+  if (l1 !== null) {
+    c.next = l1;
+  }
+
+  if (l2 !== null) {
+    c.next = l2;
+  }
+  return result.next
+};
+
+var n3 = new ListNode(4, null);
+var n2 = new ListNode(2, n3);
+var n1 = new ListNode(1, n2);
+var l1 = n1;
+
+var n6 = new ListNode(4, null);
+var n5 = new ListNode(3, n6);
+var n4 = new ListNode(1, n5);
+var l2 = n4;
+
+console.log(mergeTwoLists(l1, l2));
