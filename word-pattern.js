852e5df8c72988f2538368f741478a154f90169b
jywkim <jkim589@gmail.com>
2018-07-23 23:54:02 -0400

    word pattern solution

--- word-pattern.js
+++ word-pattern.js
@@ -31,3 +31,33 @@
 // var wordPattern = function(pattern, str) {
 //
 // };
+var wordPattern = function(pattern, str) {
+  var patternMap = {};
+  var strMap = {};
+  var ary = str.split(/\s/)
+
+  if (pattern.length != str.split(/\s/).length) {
+    return false;
+  }
+
+  for (var i in pattern) {
+    var p = pattern[i];
+    var s = ary[i];
+    if (!patternMap[p]) {
+      patternMap[p] = s;
+    } else if (patternMap[p] != s) {
+      return false;
+    }
+
+    if (!strMap[s]) {
+      strMap[s] = p;
+    }  else if (strMap[s] != p) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(wordPattern("abba", "dog cat cat dog"));
+console.log(wordPattern("abba", "dog cat cat fish"));
+console.log(wordPattern("aaaa", "dog cat cat dog"));
+console.log(wordPattern("abba", "dog dog dog dog"));
