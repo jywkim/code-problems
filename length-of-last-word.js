46cbda1654c7fab79dffd1037d92a89892fa6920
jywkim <jkim589@gmail.com>
2018-07-26 23:42:08 -0400

    length of last word solution

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,19 +1,35 @@
-QUESTION
-Length of Last Word
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-If the last word does not exist, return 0.
-
-Note: A word is defined as a character sequence consists of non-space characters only.
-
-Example:
-Input: "Hello World"
-Output: 5
-
-SOLUTION
-/**
- * @param {string} s
- * @return {number}
- */
+// QUESTION
+// Length of Last Word
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+// If the last word does not exist, return 0.
+//
+// Note: A word is defined as a character sequence consists of non-space characters only.
+//
+// Example:
+// Input: "Hello World"
+// Output: 5
+//
+// SOLUTION
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var lengthOfLastWord = function(s) {
+//
+// };
 var lengthOfLastWord = function(s) {
+  var ary = s.split(/\s/);
 
+  if (s.length == 0) return 0;
+  if (ary.length == 0) return 0;
+
+  while (ary.length > 0) {
+    var v = ary.pop();
+    if (v.length > 0) {
+      return v.length;
+    }
+  }
+  return 0;
 };
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("Length of Last Word"));
