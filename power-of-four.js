486180fb0fc83a13445ba92b4a8a9a702da501aa
jywkim <jkim589@gmail.com>
2018-07-31 22:28:06 -0400

    power of four question

---
+++ power-of-four.js
@@ -0,0 +1,17 @@
+// QUESTION
+// Power of Four
+// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+//
+// Example:
+// Given num = 16, return true. Given num = 5, return false.
+//
+// Follow up: Could you solve it without loops/recursion?
+//
+// SOLUTION
+// /**
+//  * @param {number} num
+//  * @return {boolean}
+//  */
+// var isPowerOfFour = function(num) {
+//
+// };
