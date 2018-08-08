f1b9fa76feda4f9cb296cf68476b06db380e347c
jywkim <jkim589@gmail.com>
2018-08-07 23:00:44 -0400

    add digits solution

--- add-digits.js
+++ add-digits.js
@@ -19,3 +19,16 @@
 // var addDigits = function(num) {
 //
 // };
+var addDigits = function(num) {
+  while (num >= 10) {
+    var n = num;
+    var sum = 0;
+    while (parseInt(n/10) > 0) {
+      sum += parseInt(n%10);
+      n = parseInt(n/10);
+    }
+    num = parseInt(n%10) + sum;
+  }
+  return num;
+};
+console.log(addDigits(38));
