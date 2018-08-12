6dad0092eb6bf75e75a1751af7868ab955acc8bb
jywkim <jkim589@gmail.com>
2018-08-11 22:51:52 -0400

    plus one solution

--- plus-one.js
+++ plus-one.js
@@ -22,3 +22,29 @@
 // var plusOne = function(digits) {
 //
 // };
+var plusOne = function(digits) {
+  var carry = 0;
+
+  for (var i = digits.length - 1; i >= 0; i--) {
+    digits[i] = digits[i] + carry;
+
+    if (i == digits.length - 1) {
+      digits[i] = digits[i] + 1;
+    }
+
+    if (digits[i] == 10) {
+      digits[i] = 0;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+  }
+
+  if (carry == 1) {
+    digits.unshift(carry);
+  }
+
+  return digits;
+};
+console.log(plusOne([1,2,3]));
+console.log(plusOne([4,3,2,1]));
