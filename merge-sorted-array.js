6c83f6597183956e4e64850ca7f25c5448b228f4
jywkim <jkim589@gmail.com>
2018-08-20 23:27:19 -0400

    merge sorted array solution

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -23,3 +23,22 @@
 // var merge = function(nums1, m, nums2, n) {
 //
 // };
+var merge = function(nums1, m, nums2, n) {
+  var index = 0;
+  for (var i = m; i < m+n; i++) {
+    nums1[i] = nums2[index];
+    index++;
+  }
+
+  for (var j = 0; j < nums1.length - 1; j++) {
+    for (var k = j + 1; k < nums1.length; k++) {
+      if (nums1[j] > nums1[k]) {
+        var temp = nums1[j];
+        nums1[j] = nums1[k];
+        nums1[k] = temp;
+      }
+    }
+  }
+  return nums1;
+};
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
