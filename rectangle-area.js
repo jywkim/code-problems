e5700acce21e26f40825d97de880211e6f47691d
jywkim <jkim589@gmail.com>
2018-08-24 23:46:56 -0400

    rectangle area question

---
+++ rectangle-area.js
@@ -0,0 +1,25 @@
+// QUESTION
+// Rectangle Area
+//
+// Example:
+// Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+// Output: 45
+//
+// Note:
+// Assume that the total area is never beyond the maximum possible value of int.
+//
+// SOLUTION
+// /**
+//  * @param {number} A
+//  * @param {number} B
+//  * @param {number} C
+//  * @param {number} D
+//  * @param {number} E
+//  * @param {number} F
+//  * @param {number} G
+//  * @param {number} H
+//  * @return {number}
+//  */
+// var computeArea = function(A, B, C, D, E, F, G, H) {
+//
+// };
