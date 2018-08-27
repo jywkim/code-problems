9652963bc8f23becf82320aa3e0cfcc76ade9c2c
jywkim <jkim589@gmail.com>
2018-08-26 23:51:57 -0400

    rectangle area solution

--- rectangle-area.js
+++ rectangle-area.js
@@ -23,3 +23,16 @@
 // var computeArea = function(A, B, C, D, E, F, G, H) {
 //
 // };
+var computeArea = function(A, B, C, D, E, F, G, H) {
+  var r1 =  Math.abs(A-C) * Math.abs(B-D);
+  var r2 =  Math.abs(E-G) * Math.abs(F-H);
+
+  if ( A >= G || B >= H || C <= E || D <= F) {
+    return r1 + r2;
+  }
+
+  var rD = Math.abs( (Math.max(A,E) - Math.min(C,G)) * (Math.max(B,F) - Math.min(D,H)) )
+
+  return r1 + r2 - rD;
+};
+console.log(computeArea(-3, 0, 3, 4, 0, -1, 9, 2));
